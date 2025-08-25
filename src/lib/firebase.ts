82dc4ebe3f142154b8025ad569878bdc776706ba
Yogesh Kumar Goda <yogeshkumarpraj@gmail.com>
2025-08-25 14:04:56 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  projectId: "apna-kaushal",
+  appId: "1:987931687385:web:55ed647282dfb280ed01c8",
+  storageBucket: "apna-kaushal.firebasestorage.app",
+  apiKey: "AIzaSyBwgK64Y2VQTYQtaSqFj2HEncu31RiZzig",
+  authDomain: "apna-kaushal.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "987931687385",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
