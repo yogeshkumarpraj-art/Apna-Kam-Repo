41030181c6fcbb77cbf81ec1ecdd250ebf1b1ffe
Yogesh Kumar Goda <yogeshkumarpraj@gmail.com>
2025-08-25 13:15:25 +0000

    English Language Ko Default Rako

--- src/context/language-context.tsx
+++ src/context/language-context.tsx
@@ -12,7 +12,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export const LanguageProvider = ({ children }: { children: ReactNode }) => {
-  const [language, setLanguage] = useState<Language>('hi');
+  const [language, setLanguage] = useState<Language>('en');
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
