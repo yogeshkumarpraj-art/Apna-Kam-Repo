bb71c275419bea16e6d11001f8dc660dfe5b4b5e
Yogesh Kumar Goda <yogeshkumarpraj@gmail.com>
2025-08-27 10:27:49 +0000

    aap website ki sabhi chijo ko chek karo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-
 import type {Config} from 'tailwindcss';
 
 export default {
@@ -92,33 +91,6 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
-       typography: ({ theme }: { theme: (path: string) => string }) => ({
-        DEFAULT: {
-          css: {
-            '--tw-prose-body': theme('colors.foreground'),
-            '--tw-prose-headings': theme('colors.foreground'),
-            '--tw-prose-lead': theme('colors.muted.foreground'),
-            '--tw-prose-links': theme('colors.primary.DEFAULT'),
-            '--tw-prose-bold': theme('colors.foreground'),
-            '--tw-prose-counters': theme('colors.muted.foreground'),
-            '--tw-prose-bullets': theme('colors.border'),
-            '--tw-prose-hr': theme('colors.border'),
-            '--tw-prose-quotes': theme('colors.foreground'),
-            '--tw-prose-quote-borders': theme('colors.border'),
-            '--tw-prose-captions': theme('colors.muted.foreground'),
-            '--tw-prose-code': theme('colors.foreground'),
-            '--tw-prose-pre-code': theme('colors.background'),
-            '--tw-prose-pre-bg': theme('colors.foreground'),
-            '--tw-prose-th-borders': theme('colors.border'),
-            '--tw-prose-td-borders': theme('colors.border'),
-            '--tw-prose-invert-body': theme('colors.background'),
-            '--tw-prose-invert-headings': theme('colors.background'),
-            '--tw-prose-invert-lead': theme('colors.muted.DEFAULT'),
-            '--tw-prose-invert-links': theme('colors.primary.DEFAULT'),
-            // ... and so on for other prose elements
-          },
-        },
-      }),
     },
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
