ebc15c97a2929a10eee48b0c500d7f05289c41b4
Yogesh Kumar Goda <yogeshkumarpraj@gmail.com>
2025-08-27 11:52:07 +0000

    code me to sign sahi he par display to alag p karke aa raha he

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta charSet="UTF-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
