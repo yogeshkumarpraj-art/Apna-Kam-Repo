cc8078bdd743593670f57f0aa3df5bfd07346af1
Yogesh Kumar Goda <yogeshkumarpraj@gmail.com>
2025-08-27 12:40:47 +0000

    Runtime Error
    Error: Invalid src prop (https://firebasestorage.googleap

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
