d9cd788acc954541950eeb1dc7071a1252b110f5
Yogesh Kumar Goda <yogeshkumarpraj@gmail.com>
2025-08-27 08:00:20 +0000

    ok

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,4 @@
+
 import Link from 'next/link';
 import { getSortedPostsData, PostMeta } from '@/lib/blog';
 import { Header } from '@/components/header';
--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,3 +1,4 @@
+
 export const translations = {
   en: {
     forWorkers: 'For Workers',
